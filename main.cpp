9052c8b424fd8d60bc52c66335de36079f74d7ff
abhinav772007 <abhinavs1256@gmail.com>
2026-05-25 14:05:28 +0530

    implement write/read in files

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "bitmap.hpp"
 #include "directory.hpp"
 #include <iostream>
+#include<cstring>
 using std::vector;
 using std::cout;
 using std::cin;
@@ -11,6 +12,65 @@ using std::cerr;
 using std::string;
 using std::fstream;
 
+void write_file(Disk &disk,InodeTable &it,Bitmap & bm,Directory& dir){
+    char name[32];
+    cout<<"enter filename : ";
+    cin>>name;
+    int inode_id=dir.find_entry(name);
+    if(inode_id==-1){
+        cout<<"file not found\n";
+        return;
+    }
+    Inode inode=it.read_inode(inode_id);
+    int block;
+    if(inode.direct_blocks[0]!=0){
+        block=inode.direct_blocks[0];
+    }
+    else{
+        block=bm.allocate_block();
+        if(block<0){
+            cout<<"no free blocks\n";
+            return;
+        }
+        inode.direct_blocks[0]=block;
+    }
+    cout<<"enter data : ";
+    cin.ignore();
+    string data;
+    std::getline(cin,data);
+    size_t n=data.size();
+    vector<char> buffer(Disk::BLOCK_SIZE,0);
+    memcpy(buffer.data(),data.c_str(),n);
+    disk.write_block(block,buffer);
+    inode.size=(int)n;
+    inode.used=1;
+    inode.is_dir=0;
+    it.write_inode(inode_id,inode);
+    cout<<"written...\n";
+}
+void read_file(Disk &disk,InodeTable &it,Directory& dir){
+    char name[32];
+    cout<<"\n\n\n";
+    cout<<"-----------------------------\n";
+    cout<<"enter filename : ";
+    cin>>name;
+    int inode_id=dir.find_entry(name);
+    if(inode_id==-1){
+        cout<<"file not found\n";
+        return;
+    }
+    Inode inode=it.read_inode(inode_id);
+    if(inode.direct_blocks[0]==0){
+        cout<<"file is empty...\n";
+        return;
+    }
+    vector<char> buffer;
+    disk.read_block(inode.direct_blocks[0],buffer);
+    string data(buffer.begin(),buffer.begin()+inode.size);
+    cout<<"file content: "<<data<<"\n";
+    cout<<"-----------------------------\n";
+    cout<<"\n\n";
+}
 int main(){
     try{
         Disk disk("disk.img");
@@ -31,9 +91,9 @@ int main(){
         }
         fs.debug();
         const superblock& sb=fs.get_superblock();
-        //int bitmap_block=sb.inode_start+sb.inode_blocks;
-        // Bitmap bm(disk,11,1024);
-        // bm.load();
+        int bitmap_block=sb.inode_start+sb.inode_blocks;
+        Bitmap bm(disk,bitmap_block,sb.total_blocks);
+        bm.load();
         InodeTable it(disk,sb.inode_start);
         Inode root=it.read_inode(0);
         Directory dir(disk,root.direct_blocks[0]);
@@ -43,6 +103,8 @@ int main(){
             cout<<"create file ---> 1\n";
             cout<<"list files ---> 2\n";
             cout<<"exit ----> 3\n";
+            cout<<"write in file --->4\n";
+            cout<<"read from file -->5\n";
             int op;
             cin>>op;
             if(op==3){
@@ -66,6 +128,12 @@ int main(){
             }
             else if(op==2){
                 dir.list();
+            }
+            else if(op==4){
+                write_file(disk,it,bm,dir);
+            }
+            else if(op==5){
+                read_file(disk,it,dir);
             }
             else{
                 cerr<<"invalid option\n";
